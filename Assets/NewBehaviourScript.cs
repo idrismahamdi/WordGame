3069d03c53af14ab3b01713fb4c6d286fdf24e48
Emil-Harvey <58564095+Emil-Harvey@users.noreply.github.com>
2022-03-05 14:05:24 +0000

    Add files via upload

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    ///public List<GameObject> Children;
+    public Text WordToGuess;
+    public InputField InputWord;
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+         foreach (Transform child in transform)
+         {
+             if (child.tag == "Word")
+             {
+               // WordToGuess = (Text)child.gameObject;
+                
+             }
+             else if (child.tag == "Input")
+             {
+                //InputWord = child.gameObject;
+                
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
